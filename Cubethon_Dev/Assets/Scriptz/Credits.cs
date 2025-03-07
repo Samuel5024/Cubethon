9d6a7185f267d2896b87f6cc77b495109d222bb7
Samuel Fuhrman <148499762+Samuel5024@users.noreply.github.com>
2025-03-07 13:51:00 -0700

    extra level & restart button

--- Cubethon_Dev/Assets/Scriptz/Credits.cs
+++ Cubethon_Dev/Assets/Scriptz/Credits.cs
@@ -1,10 +1,10 @@
 using UnityEngine;
+using UnityEngine.SceneManagement;
 
 public class Credits : MonoBehaviour
 {
-    public void Quit()
+    public void Restart()
     {
-        Debug.Log("I quit!");
-        Application.Quit();
+        SceneManager.LoadScene(0);
     }
 }
